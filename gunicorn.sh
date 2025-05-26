18560f700dd14cb917dfed35837b55c46dc234da
Célio Júnior <celiojr.dev@gmail.com>
2025-05-26 16:25:34 -0300

    updated gunicorn

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
-# gunicorn backend.asgi:application \
-#     --worker-class uvicorn.workers.UvicornWorker \
-#     --workers 2 \
-#     --bind 0.0.0.0:8000 \
-#     --timeout 60 \
-#     --keep-alive 5
-
 gunicorn backend.asgi:application \
-    --worker-class uvicorn.workers.UvicornWorker \
-    --workers 1 \
-    --threads 1 \
-    --bind 0.0.0.0:8000 \
-    --timeout 30 \
-    --keep-alive 2 \
-    --access-logfile /dev/null \
-    --error-logfile /dev/null \
-    --log-level critical # info
+  --worker-class uvicorn.workers.UvicornWorker \
+  --workers 1 \
+  --threads 1 \
+  --bind 0.0.0.0:8000 \
+  --timeout 30 \
+  --keep-alive 1 \
+  --log-level warning
