1ffcaac3d31c5e2ef045809f724e03575055328d
Célio Júnior <celiojr.dev@gmail.com>
2025-05-26 16:38:58 -0300

    usando apenas uvcorn

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-gunicorn backend.asgi:application \
-  --worker-class uvicorn.workers.UvicornWorker \
-  --workers 1 \
-  --threads 1 \
-  --bind 0.0.0.0:8000 \
-  --timeout 30 \
-  --keep-alive 1 \
-  --log-level critical
+# gunicorn backend.asgi:application \
+#   --worker-class uvicorn.workers.UvicornWorker \
+#   --workers 1 \
+#   --threads 1 \
+#   --bind 0.0.0.0:8000 \
+#   --timeout 30 \
+#   --keep-alive 1 \
+#   --log-level critical
+
+uvicorn backend.asgi:application --host 0.0.0.0 --port 8000 --workers 1
