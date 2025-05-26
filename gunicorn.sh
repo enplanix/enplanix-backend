5a8931099a2c858d8a5ce2bacf13b7da2b7b2eca
Célio Júnior <celiojr.dev@gmail.com>
2025-05-26 15:06:40 -0300

    alteração de workers

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 gunicorn backend.asgi:application \
     --worker-class uvicorn.workers.UvicornWorker \
-    --workers 3 \
+    --workers 2 \
     --bind 0.0.0.0:8000 \
     --timeout 60 \
     --keep-alive 5
