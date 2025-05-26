d14f589920f1745ccebe971cf04e1c48d6cde975
Célio Júnior <celiojr.dev@gmail.com>
2025-05-26 13:56:04 -0300

    otimização de workers

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 gunicorn backend.asgi:application \
     --worker-class uvicorn.workers.UvicornWorker \
-    --workers 1 \
+    --workers 4 \
     --threads 2 \
     --bind 0.0.0.0:8000 \
     --timeout 60 \
