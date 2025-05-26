61a76d18703b56b3284cb003a0443463e7664724
Célio Júnior <celiojr.dev@gmail.com>
2025-05-26 15:51:55 -0300

    alterações de configuração de servidor

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-gunicorn backend.asgi:application \
-    --worker-class uvicorn.workers.UvicornWorker \
-    --workers 2 \
+# gunicorn backend.asgi:application \
+#     --worker-class uvicorn.workers.UvicornWorker \
+#     --workers 2 \
+#     --bind 0.0.0.0:8000 \
+#     --timeout 60 \
+#     --keep-alive 5
+
+# single thread
+gunicorn backend.wsgi:application \
+    --worker-class gthread \
+    --workers 1 \
+    --threads 4 \
     --bind 0.0.0.0:8000 \
     --timeout 60 \
     --keep-alive 5
-
