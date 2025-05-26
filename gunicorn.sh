149637a774fba1d06dd2e780600f8ac9afa2e4b8
Célio Júnior <celiojr.dev@gmail.com>
2025-05-26 15:00:21 -0300

    adição de mais workers

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 gunicorn backend.asgi:application \
     --worker-class uvicorn.workers.UvicornWorker \
-    --workers 1 \
-    --threads 4 \
+    --workers 3 \
     --bind 0.0.0.0:8000 \
     --timeout 60 \
     --keep-alive 5
