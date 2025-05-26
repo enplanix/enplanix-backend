ada96460b99a9bf073819cbcef15b74bc6ec2700
Célio Júnior <celiojr.dev@gmail.com>
2025-05-26 10:58:37 -0300

    otimização do gunicorn

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
-python manage.py migrate
+# When you change hardware, check workers-help.txt
 
 gunicorn backend.asgi:application \
     --worker-class uvicorn.workers.UvicornWorker \
-    --bind 0.0.0.0:8000
+    --workers 1 \
+    --threads 2 \
+    --bind 0.0.0.0:8000 \
+    --timeout 60 \
+    --keep-alive 5
+
